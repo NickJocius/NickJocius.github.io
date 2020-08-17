6c4bb4b762342595b5e1660cbed89f6d967453cc
Nick Jocius <capacity512@yahoo.com>
2020-08-17 17:32:10 -0500

    rewrite of app.js and css
    rewrote app.js.  Added jquery events for buttons and hover effects.  Fixed css issues and added animations keyframes.

--- js/app.js
+++ js/app.js
@@ -2,8 +2,40 @@
 
 $(function() {
     
-    setTimeout(function() {
-        $('proceed button').slideToggle("slow");
-    },100000);
+    let btn = $(".proceed_btn");
+    let title = $(".section1_h1 span");
+    let author = $(".section2_h2");
 
-})
+    $(btn).hide();
+
+    setTimeout(function(){
+        $(btn).show('slow');
+        $(btn).removeClass('shadow-none');
+        $(btn).addClass('glowing');
+    }, 10000);
+
+    $(btn).on("mouseover", function(){
+        $(this).css("transform", "rotate(5deg)");
+    })
+    .on("mouseout", function(){
+        $(this).css("transform", "rotate(0deg)");
+    })
+
+    $(btn)
+    .on("click", function(){
+        console.log("clicked");
+        $(author).addClass('authorAnimation');
+        $(title).addClass('showAnimation');
+        $(btn).hide();
+    });
+    
+
+    $(title)
+    .on("mouseover", function(){
+        $(this).css("text-shadow", "0 0 10px #fff, 0 0 20px #fff, 0 0 30px #0073e6, 0 0 40px #0073e6, 0 0 50px #0073e6, 0 0 60px #0073e6, 0 0 70px #0073e6")
+    })
+    .on("mouseout", function(){
+        $(this).css("text-shadow","none");
+    });
+});
+
